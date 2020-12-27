e421a10c1507f851c8a62afc0738f040e3e23100
ak5901 <33071745+ak5901@users.noreply.github.com>
2020-12-27 02:23:11 -0500

    Added Map to AppJS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navbar from './views/Navbar';
 import Register from './views/Register';
 import Login from './views/Login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './styling/style.css';
+import { Map, GoogleApiWrapper} from 'google-maps-react';
 
+const mapStyles = {
+	width: '100%',
+	height: '100%'
+};
+
+export class MapContainer extends Component {
+	render() {
+		return(
+			<Map
+			google={this.props.google}
+			zoom= {14}
+			style= {mapStyles}
+			initialCenter = {
+				{
+					lat: -10,
+					lng: 20
+				}
+			}
+		/>
+	);
+}
+}
+	
+
+		
 function App() {
 	return (
 		<Router>
@@ -14,7 +40,11 @@ function App() {
 			<Route path='/login' component={Login} />;
 			</div>
 		</Router>
+		
 	);
 }
 
-export default App;
+// export default App;
+export default GoogleApiWrapper({
+	apiKey: 'AIzaSyDLiQIfQF-jxb4bbMMEC2TX9Xn-zLKLSYM'
+})(MapContainer);
