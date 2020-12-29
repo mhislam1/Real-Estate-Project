c0b69c6180d65d8db9a1bb1a40ed165fd46376fe
Mohammad Islam <mhislam@buffalo,edu>
2020-12-28 21:13:46 -0500

    Added a Bootstrap navbar and changed component name to NavigationBar. Currently working on displaying the logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Navbar from './views/Navbar';
+import NavigationBar from './views/Navbar';
 import Register from './views/Register';
 import Login from './views/Login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './styling/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
-			<Navbar />
+			<NavigationBar />
 			<Route path='/register' component={Register} />;
 			<Route path='/login' component={Login} />;
 			</div>
--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -3,14 +3,17 @@ import React from 'react';
 import logo from '../styling/Image/house.jpg';
 import landing from '../styling/Image/landing.jpeg';
 import background from '../styling/Image/background1.jpeg'
+import { Navbar, Nav} from 'react-bootstrap';
 
-
-export default function Navbar() {
+export default function NavigationBar() {
 	return (
 		<>
 		<body >
 		<header>
-			<div class="container">
+			
+			{/* The following code produced the old landing page */}
+			
+			{/* <div class="container">
 				<a href="/"><img src={logo} alt="logo" /></a>
 			</div>
 
@@ -22,7 +25,24 @@ export default function Navbar() {
 				<li><a class='nav-link' href='login'>Login</a></li>
 				<li><a class='nav-link' href='about'>About</a></li>
 				<li><a class='nav-link' href='contacts'>Contact</a></li>
-			</nav>
+			</nav> */}
+			  <Navbar expand="lg" bg="dark" variant="dark">
+				<Navbar.Brand href="#home">
+				<img
+					src= '../styling/Image/house.jpg'
+					width="30"
+					height="30"
+					className="d-inline-block align-top"
+					// alt="React Bootstrap logo"
+				/>
+				Fillow
+				</Navbar.Brand>
+				<Nav className="mr-auto">
+				<Nav.Link href="#home">Home</Nav.Link>
+				<Nav.Link href="#features">Features</Nav.Link>
+				<Nav.Link href="#pricing">Pricing</Nav.Link>
+				</Nav>
+			</Navbar>
 		</header>
 		<div class ='background-div'>
 			<img src={landing} class='background' alt="comp" />
