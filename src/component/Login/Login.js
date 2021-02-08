595be98e60a9a3a5fea561c8932aa78404c0d3ce
tsefat2143 <tajdidul.sefat@gmail.com>
2021-02-08 16:04:06 -0500

    Form Styling

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 export default function Login(){
@@ -8,13 +9,15 @@ return(
         <div class="form-app">
 			<h1>Log In To Your Account </h1>
 				
-			Username:
+			<label>Username:</label>
 			<input type="text" class="form-text" placeholder="Enter Username" name="username" required/><br />
 				
-			Password:
+			<label>Password:</label>
 			<input type="password" class="form-text" minlength="8" placeholder="Enter Password" name="password" required/><br />
 				
 			<input type="submit" value="Sign in" class="reg-button" id="button1" /> <br />
+
+			<p>Need an account? <Link to='/register'>Click here</Link></p>
         </div>
 	</div>
     </>
--- src/component/Register/Register.js
+++ src/component/Register/Register.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import landing from '../../Image/background2.jpg'
+import landing from '../../Image/background2.jpg';
+import {Link} from 'react-router-dom';
 import './Register.css'
 
 export default function Navbar() {
@@ -9,28 +10,28 @@ export default function Navbar() {
             <div className="form-app">
 				<h1>Register</h1>
 
-				First Name:
+				<label>First Name:</label>
 				<input type="text" className="form-text" placeholder="Enter First Name" name="first" required/><br />
 				
-				Last Name:
+				<label>Last Name:</label>
 				<input type="text" className="form-text" placeholder="Enter Last Name" name="last" required/><br />
 				
-				Middle Initial: (Optional)
+				<label>Middle Initial: (Optional)</label>
 				<input type="text" className="form-text" placeholder="Enter Middle Name" name="middle"/><br />
 				
-				Username:
+				<label>Username:</label>
 				<input type="text" className="form-text" placeholder="Enter Username" name="username" required/><br />
 				
-				Password:
+				<label>Password:</label>
 				<input type="password" className="form-text" minlength="8" placeholder="Enter Password" name="password" required/><br />
 
-				Re-enter Password:
+				<label>Re-enter Password:</label>
 				<input type="password" className="form-text" minlength="8" placeholder="Enter Password" name="password2" required/><br />				
 
-				Email:
+				<label>Email:</label>
 				<input type="email" className="form-text" placeholder="Enter Email" name="email" required/><br />
 				
-				Gender:<br />
+				<label>Gender:</label><br />
 				<div className="radio">
 				<input type="radio" className="form-text" name="gender" value="M" required/>Male<br />
 				<input type="radio" className="form-text" name="gender" value="F" required/>Female<br />
@@ -39,6 +40,8 @@ export default function Navbar() {
 
 				<input type="submit" value="Register" className="reg-button" id="button1"  />
 				<input type="Reset" className="reset-button" id="button2" /><br />
+
+				<p>Already have an account? <Link to='/login'>Click here</Link></p>
 			</div>
 		</div>
 		</>
